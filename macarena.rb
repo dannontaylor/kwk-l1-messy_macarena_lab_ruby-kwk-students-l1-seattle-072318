61043aef643815a091ebae0e1dfb61b34ad97e86
dannontaylor <taylordannon@gmail.com>
2018-07-30 16:10:04 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -2,26 +2,26 @@ puts "How To Do The Macarena:"
 
 ##Steps of the Macarena
 
-# Step 1: GOOD
+# Step 1:
 puts "Step 1: Right arm out in front of you, palm facing down."
 
-# Step 2: GOOD
+# Step 2:
 puts "Step 2: Left arm out in front of you, palm facing down."
 
-# Step 3: GOOD
+# Step 3:
 puts "Step 3: Turn your right palm up."
 
-# Step 4: GOOD
+# Step 4:
 puts "Step 4: Turn your left palm up."
 
-# Step 5: GOOD
+# Step 5:
 puts "Step 5: Place your right hand on your left shoulder."
 
-# Step 6: GOOD
+# Step 6:
 puts "Step 6: Place your left hand on your right shoulder."
 
-# Step 7: GOOD
+# Step 7:
 puts "Step 7: Bring your right hand up and around to the back of your head."
 
-# Step 8: GOOD
+# Step 8:
 puts "Step 8: Bring your left hand up and around to the back of your head."
